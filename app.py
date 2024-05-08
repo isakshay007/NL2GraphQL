b2e05ff41d7ee3b7720fc7ff36e770211dae7ad1
isakshay007 <117896902+isakshay007@users.noreply.github.com>
2024-05-07 18:59:19 -0700

    Adding Files

---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+from lyzr_automata.ai_models.openai import OpenAIModel
+from lyzr_automata import Agent, Task
+from lyzr_automata.pipelines.linear_sync_pipeline import LinearSyncPipeline
+from PIL import Image
+from lyzr_automata.tasks.task_literals import InputType, OutputType
+import os
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = st.secrets["apikey"]
+
+st.markdown(
+    """
+    <style>
+    .app-header { visibility: hidden; }
+    .css-18e3th9 { padding-top: 0; padding-bottom: 0; }
+    .css-1d391kg { padding-top: 1rem; padding-right: 1rem; padding-bottom: 1rem; padding-left: 1rem; }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+image = Image.open("./logo/lyzr-logo.png")
+st.image(image, width=150)
+
+# App title and introduction
+st.title("NL2GraphQL")
+st.markdown("Built using Lyzr SDK🚀")
+st.markdown("Welcome to NL2GraphQL! Translate natural language prompts into accurate GraphQL queries effortlessly.")
+input = st.text_input("Please enter your natural language prompt:",placeholder=f"""Type here""")
+
+open_ai_text_completion_model = OpenAIModel(
+    api_key=st.secrets["apikey"],
+    parameters={
+        "model": "gpt-4-turbo-preview",
+        "temperature": 0.2,
+        "max_tokens": 1500,
+    },
+)
+
+
+def generation(input):
+    generator_agent = Agent(
+        role=" Expert GRAPH DATABASE ENGINEER and ANALYST",
+        prompt_persona=f"Your task is to TRANSLATE a natural language prompt into the corresponding GRAPHQL query, ensuring that it FULLY CAPTURES the user's intent expressed in natural language.")
+
+    prompt = f"""
+You are an Expert GRAPH DATABASE ENGINEER and ANALYST. Your task is to TRANSLATE a natural language prompt into the corresponding GRAPHQL query, ensuring that it FULLY CAPTURES the user's intent expressed in natural language.
+
+Follow these steps for SUCCESSFUL execution:
+
+1. Carefully READ and UNDERSTAND the natural language prompt provided by the user, identifying KEY COMPONENTS such as requested entities, attributes, and relationships.
+
+2. CONVERT these components into a structured GRAPHQL query, paying close attention to SYNTAX and SEMANTICS that reflect the user’s intent.
+
+3. Before DISPLAYING the GRAPHQL  query, VERIFY its relevance by cross-referencing it with the original user input to ENSURE ACCURACY.
+
+You MUST provide an EXPLANATION for each segment of your query to help users understand how their natural language input was interpreted.
+
+ """
+
+    generator_agent_task = Task(
+        name="Generation",
+        model=open_ai_text_completion_model,
+        agent=generator_agent,
+        instructions=prompt,
+        default_input=input,
+        output_type=OutputType.TEXT,
+        input_type=InputType.TEXT,
+    ).execute()
+
+    return generator_agent_task 
+   
+if st.button("Convert"):
+    solution = generation(input)
+    st.markdown(solution)
+
+with st.expander("ℹ️ - About this App"):
+    st.markdown("""
+    This app uses Lyzr Automata Agent . For any inquiries or issues, please contact Lyzr.
+
+    """)
+    st.link_button("Lyzr", url='https://www.lyzr.ai/', use_container_width=True)
+    st.link_button("Book a Demo", url='https://www.lyzr.ai/book-demo/', use_container_width=True)
+    st.link_button("Discord", url='https://discord.gg/nm7zSyEFA2', use_container_width=True)
+    st.link_button("Slack",
+                   url='https://join.slack.com/t/genaiforenterprise/shared_invite/zt-2a7fr38f7-_QDOY1W1WSlSiYNAEncLGw',
+                   use_container_width=True)
